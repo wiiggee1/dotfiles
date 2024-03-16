26b764e9212729e6a00368c8b7f360f48907a5b0
wiiggee1 <viksme-9@student.ltu.se>
2024-03-16 14:10:56 +0100

    dotfiles initial

---
+++ nvim/lua/custom/smart_window.lua
@@ -0,0 +1,13 @@
+local api = vim.api
+local cmd = vim.cmd
+
+local SmartWindow = {}
+
+function SmartWindow.create_window()
+    -- Do something
+    print("Hello new Plugin!")
+    vim.print("Hello new Plugin!")
+
+end
+
+return SmartWindow
